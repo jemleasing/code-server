c6bc0e10604c20a9d147f86265a3b3efbcb495c0
jemleasing <info@jemleasing.com>
2026-09-02 03:16:45 -0400

    Removed c.LicensePlate

--- backend/app/routers/leases.py
+++ backend/app/routers/leases.py
@@ -17,7 +17,7 @@ def get_active_full_report(db: Session = Depends(get_connection)) -> List[Dict[s
         SELECT 
             l.ID, l.`Lease#`, l.Customer, l.Phone, l.AmountDue, l.LastPayDate, l.Coll_Status,
             l.Active, -- Added this so we can inspect the actual value
-            c.Make, c.Model, c.Year, c.LicensePlate,
+            c.Make, c.Model, c.Year
             cust.EstMilesPerWeek, cust.BaseName
         FROM tbllease l
         LEFT JOIN tbl_b_car c ON l.VIN = c.Vin
