b9076c72cd00a58d63243d6d67aec108511b8674
jemleasing <info@jemleasing.com>
2026-09-02 03:26:53 -0400

    Edit EstimatedWeeklyMilesCarWillBeDriven

--- backend/app/routers/leases.py
+++ backend/app/routers/leases.py
@@ -18,7 +18,7 @@ def get_active_full_report(db: Session = Depends(get_connection)) -> List[Dict[s
             l.ID, l.`Lease#`, l.Customer, l.Phone, l.AmountDue, l.LastPayDate, cust.CollStatus,
             l.Active, -- Added this so we can inspect the actual value
             c.Make, c.Model, c.Year
-            cust.EstMilesPerWeek, cust.BaseName
+            cust.EstimatedWeeklyMilesCarWillBeDriven, cust.BaseName
         FROM tbllease l
         LEFT JOIN tbl_b_car c ON l.VIN = c.Vin
         LEFT JOIN tbl_b_customer cust ON l.`Lease#` = cust.`Lease#`
